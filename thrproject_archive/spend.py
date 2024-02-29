131594e7a0af3346aaa9afa5a1ae15d2ea0b5a0b
asgurbuz <asgurbuz46@gmail.com>
2024-02-29 14:57:50 +0300

    Delete thrproject_archive directory

--- thrproject_archive/spend.py
+++
@@ -1,5 +0,0 @@
-from blockcypher import *
-def spend(your_priv,to_address,amount):
-    simple_spend(from_privkey="d7e1b11bb973a41c7fe743ace61ed978196b5387d12f66ea4cabb10eaebd72b7",
-             to_address="mkHS9ne12qx9pS9VojpwU5xtRd4T7X7ZUt",to_satoshis=15000,privkey_is_compressed=False,
-             api_key="872ecdba674d427c9ecdd53446beea7f",coin_symbol="btc-testnet")
